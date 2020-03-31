58aaf3d0b5f5ff14779e152e1e83ff84e70e19dc
Renato Violin <renato@MacBook-Pro.local>
2020-03-31 16:02:52 -0300

    parameters fixed

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ import json
 from transformers import BartTokenizer, BartForConditionalGeneration, BartConfig
 from transformers import T5Tokenizer, T5ForConditionalGeneration, T5Config
 
-BART_PATH = 'bart-base'
-T5_PATH = 't5-base'
-# BART_PATH = 'model/bart'
-# T5_PATH = 'model/T5'
+# BART_PATH = 'bart-large'
+# T5_PATH = 't5-base'
+BART_PATH = 'model/bart'
+T5_PATH = 'model/T5'
 
 app = Flask(__name__)
 bart_model = BartForConditionalGeneration.from_pretrained(BART_PATH)
@@ -20,22 +20,34 @@ t5_tokenizer = T5Tokenizer.from_pretrained(T5_PATH)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-def bart_summarize(input_text, num_beams=6, num_words=50):
+def bart_summarize(input_text, num_beams=4, num_words=50):
     input_text = str(input_text)
     input_text = ' '.join(input_text.split())
     input_tokenized = bart_tokenizer.encode(input_text, return_tensors='pt')
-    summary_ids = bart_model.generate(input_tokenized, num_beams=int(num_beams), max_length=int(num_words), early_stopping=True)
+    summary_ids = bart_model.generate(input_tokenized,
+                                      num_beams=int(num_beams),
+                                      no_repeat_ngram_size=3,
+                                      length_penalty=2.0,
+                                      min_length=30,
+                                      max_length=int(num_words),
+                                      early_stopping=True)
     output = [bart_tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=False) for g in summary_ids]
     return output[0]
 
 
-def t5_summarize(input_text, num_beams=6, num_words=50):
+def t5_summarize(input_text, num_beams=4, num_words=50):
     input_text = str(input_text).replace('\n', '')
     input_text = ' '.join(input_text.split())
     input_tokenized = t5_tokenizer.encode(input_text, return_tensors="pt")
     summary_task = torch.tensor([[21603, 10]])
     input_tokenized = torch.cat([summary_task, input_tokenized], dim=-1)
-    summary_ids = t5_model.generate(input_tokenized, num_beams=int(num_beams), max_length=int(num_words), early_stopping=False)
+    summary_ids = t5_model.generate(input_tokenized,
+                                    num_beams=int(num_beams),
+                                    no_repeat_ngram_size=3,
+                                    length_penalty=2.0,
+                                    min_length=30,
+                                    max_length=int(num_words),
+                                    early_stopping=True)
     output = [t5_tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=False) for g in summary_ids]
     return output[0]
 
