064dae5b9276719acc7ea597dedfe0588b5c3509
Renato Violin <renato@MacBook-Pro.local>
2020-04-02 12:01:26 -0300

    added .to(device) on tensors

--- app.py
+++ app.py
@@ -6,14 +6,15 @@ import json
 from transformers import BartTokenizer, BartForConditionalGeneration, BartConfig
 from transformers import T5Tokenizer, T5ForConditionalGeneration, T5Config
 
-BART_PATH = 'bart-large'
-T5_PATH = 't5-base'
-# BART_PATH = 'model/bart'
-# T5_PATH = 'model/T5'
+# BART_PATH = 'bart-large'
+# T5_PATH = 't5-base'
+BART_PATH = 'model/bart'
+T5_PATH = 'model/T5'
 
 app = Flask(__name__)
 bart_model = BartForConditionalGeneration.from_pretrained(BART_PATH)
 bart_tokenizer = BartTokenizer.from_pretrained(BART_PATH)
+
 
 t5_model = T5ForConditionalGeneration.from_pretrained(T5_PATH)
 t5_tokenizer = T5Tokenizer.from_pretrained(T5_PATH)
@@ -23,7 +24,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 def bart_summarize(input_text, num_beams=4, num_words=50):
     input_text = str(input_text)
     input_text = ' '.join(input_text.split())
-    input_tokenized = bart_tokenizer.encode(input_text, return_tensors='pt')
+    input_tokenized = bart_tokenizer.encode(input_text, return_tensors='pt').to(device)
     summary_ids = bart_model.generate(input_tokenized,
                                       num_beams=int(num_beams),
                                       no_repeat_ngram_size=3,
@@ -38,9 +39,9 @@ def bart_summarize(input_text, num_beams=4, num_words=50):
 def t5_summarize(input_text, num_beams=4, num_words=50):
     input_text = str(input_text).replace('\n', '')
     input_text = ' '.join(input_text.split())
-    input_tokenized = t5_tokenizer.encode(input_text, return_tensors="pt")
-    summary_task = torch.tensor([[21603, 10]])
-    input_tokenized = torch.cat([summary_task, input_tokenized], dim=-1)
+    input_tokenized = t5_tokenizer.encode(input_text, return_tensors="pt").to(device)
+    summary_task = torch.tensor([[21603, 10]]).to(device)
+    input_tokenized = torch.cat([summary_task, input_tokenized], dim=-1).to(device)
     summary_ids = t5_model.generate(input_tokenized,
                                     num_beams=int(num_beams),
                                     no_repeat_ngram_size=3,
